d7afb74e42509adbecf1b0622d75fc107a3f210f
bleylek <bartuleylek88@gmail.com>
2026-07-29 21:54:46 +0300

    Arduino IDE comments removed

--- Alternating_LEDs.ino
+++ Alternating_LEDs.ino
@@ -2,14 +2,14 @@
 #define green_led 9
 
 void setup() {
-  // put your setup code here, to run once:
+  
   pinMode(red_led, OUTPUT); // Set pin 8 and 9 as a digital output.
   pinMode(green_led, OUTPUT);
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  
   digitalWrite(red_led, HIGH);
   digitalWrite(green_led, LOW);
   delay(500);
